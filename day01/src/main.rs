b474aa308d4c4883f4db11ee6d49e738acfe5b0d
Orvar Segerström <orvarsegerstrom@gmail.com>
2018-12-01 19:45:55 +0100

    Day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("input.txt");
     let changes: Vec<isize> = input
@@ -5,7 +7,20 @@ fn main() {
         .split("\n")
         .map(|x| parse(x))
         .collect::<Vec<_>>();
+
     println!("Part 1: {}", changes.iter().sum::<isize>());
+
+    let mut visited_frequencies = HashSet::new();
+    visited_frequencies.insert(0isize);
+    let mut current_frequency = 0isize;
+    for delta in changes.iter().cycle() {
+        current_frequency += delta;
+        if visited_frequencies.contains(&current_frequency) {
+            break;
+        }
+        visited_frequencies.insert(current_frequency);
+    }
+    println!("Part 2: {}", current_frequency);
 }
 
 fn parse(line: &str) -> isize {
