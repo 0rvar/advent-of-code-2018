12033c569856d2293e854c0b5989b6f4487021d5
Orvar Segerström <orvarsegerstrom@gmail.com>
2018-12-05 08:39:30 +0100

    Day 5 cleanup and performance squeezing

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,97 +1,75 @@
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn main() {
-    let input = include_str!("input.txt").trim();
-    // println!("Part 1: {}", polymer_reaction(input).len());
+    let input = include_str!("input.txt")
+        .trim()
+        .bytes()
+        .collect::<Vec<u8>>();
+    println!("Part 1: {}", polymer_reaction(&input).len());
 
-    for unit_to_remove in find_units(input) {
-        let filtered = input
-            .chars()
-            .filter(|&x| x != unit_to_remove)
-            .collect::<String>();
-        println!(
-            "Part 2: removing {} gives len {}",
-            unit_to_remove,
+    let lengths = find_units(&input)
+        .iter()
+        .map(|c| {
+            let filtered = input
+                .iter()
+                .filter(|&x| x.to_ascii_lowercase() != *c)
+                .map(|x| *x)
+                .collect::<Vec<u8>>();
             polymer_reaction(&filtered).len()
-        );
-    }
+        })
+        .collect::<Vec<usize>>();
+    println!("Part 2: min length {}", lengths.iter().min().unwrap());
 }
 
-fn find_units(polymer: &str) -> Vec<char> {
-    let mut pairs: HashSet<char> = HashSet::new();
-    for x in polymer.chars() {
+fn find_units(polymer: &[u8]) -> Vec<u8> {
+    let mut pairs: HashSet<u8> = HashSet::new();
+    for x in polymer {
         pairs.insert(x.to_ascii_lowercase());
     }
 
-    pairs.iter().map(|x| *x).collect::<Vec<char>>()
+    pairs.iter().map(|x| *x).collect::<Vec<u8>>()
 }
 
-fn polymer_reaction(polymer: &str) -> String {
-    let polymer_len = polymer.len();
-    if polymer_len < 1 {
-        return "".to_string();
-    }
-    let mut removed_indices = HashSet::new();
-    let mut char_lookup = HashMap::new();
-    for (i, x) in polymer.chars().enumerate() {
-        char_lookup.insert(i, x);
-    }
-    'outer: loop {
-        'scan: for i in 0..(polymer_len - 1) {
-            if removed_indices.contains(&i) {
-                continue;
-            }
-            let mut j: usize = i + 1;
-            for index in (i + 1)..polymer_len {
-                if !removed_indices.contains(&index) {
-                    j = index;
-                    break;
-                }
-                if index == polymer_len - 1 {
-                    continue 'scan;
-                }
-            }
-            let this = char_lookup.get(&i).unwrap();
-            let next = char_lookup.get(&j).unwrap();
-            if units_will_react(*this, *next) {
-                removed_indices.insert(i);
-                removed_indices.insert(j);
-                continue 'outer;
-            }
+fn polymer_reaction(polymer: &[u8]) -> Vec<u8> {
+    let mut result: Vec<u8> = vec![];
+    for c in polymer {
+        if result.is_empty() {
+            result.push(*c);
+            continue;
         }
-        break 'outer;
+        if units_will_react(*c, *result.last().unwrap()) {
+            result.pop();
+        } else {
+            result.push(*c);
+        }
     }
 
-    polymer
-        .chars()
-        .enumerate()
-        .filter(|(index, _)| !removed_indices.contains(index))
-        .map(|(_, x)| x)
-        .collect::<String>()
+    result
 }
 
 #[test]
 fn test_polymer_reaction() {
-    assert_eq!(polymer_reaction("Aa"), "".to_string());
-    assert_eq!(polymer_reaction("aA"), "".to_string());
-    assert_eq!(polymer_reaction("abBA"), "".to_string());
-    assert_eq!(polymer_reaction("aabAAB"), "aabAAB".to_string());
+    fn polymer_reaction_string(s: &str) -> String {
+        polymer_reaction(&s.bytes().collect::<Vec<_>>())
+            .iter()
+            .map(|x| *x as char)
+            .collect::<String>()
+    }
+    assert_eq!(polymer_reaction_string("Aa"), "".to_string());
+    assert_eq!(polymer_reaction_string("aA"), "".to_string());
+    assert_eq!(polymer_reaction_string("abBA"), "".to_string());
+    assert_eq!(polymer_reaction_string("aabAAB"), "aabAAB".to_string());
 }
 
-fn units_will_react(a: char, b: char) -> bool {
-    return (a.to_ascii_lowercase() == a
-        && b.to_ascii_uppercase() == b
-        && a.to_ascii_uppercase() == b)
-        || (a.to_ascii_uppercase() == a
-            && b.to_ascii_lowercase() == b
-            && a.to_ascii_lowercase() == b);
+#[inline]
+fn units_will_react(a: u8, b: u8) -> bool {
+    return (a as isize - b as isize).abs() == b'a' as isize - b'A' as isize;
 }
 
 #[test]
 fn test_units_will_react() {
-    assert_eq!(units_will_react('a', 'A'), true);
-    assert_eq!(units_will_react('A', 'a'), true);
-    assert_eq!(units_will_react('a', 'b'), false);
-    assert_eq!(units_will_react('a', 'B'), false);
+    assert_eq!(units_will_react(b'a', b'A'), true);
+    assert_eq!(units_will_react(b'A', b'a'), true);
+    assert_eq!(units_will_react(b'a', b'b'), false);
+    assert_eq!(units_will_react(b'a', b'B'), false);
 }
