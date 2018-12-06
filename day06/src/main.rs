e6367a081975b768081c71c8136f2147ddc3d141
Orvar Segerström <orvarsegerstrom@gmail.com>
2018-12-06 06:40:58 +0100

    Day 6 part 1

---
+++ day06/src/main.rs
@@ -0,0 +1,87 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Position {
+    x: isize,
+    y: isize,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Claim {
+    id: isize,
+    distance: usize,
+}
+
+fn main() {
+    let input = include_str!("input.txt")
+        .trim()
+        .split("\n")
+        .map(|x| {
+            let parts = x
+                .split(", ")
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<isize>>();
+            (parts[0], parts[1])
+        })
+        .collect::<Vec<_>>();
+
+    let mut map: HashMap<Position, Claim> = HashMap::new();
+    let mut region_sizes: HashMap<usize, usize> = HashMap::new();
+    let mut infinite_area_ids = HashSet::new();
+    let min_x = -100;
+    let min_y = -100;
+    let max_x = 1000;
+    let max_y = 1000;
+    for x in min_x..=max_x {
+        for y in min_y..=max_y {
+            let mut claim = Claim {
+                id: -1,
+                distance: 100000,
+            };
+            for (index, coordinate) in input.iter().enumerate() {
+                let distance: usize = manhattan_distance(coordinate.0, coordinate.1, x, y);
+                if distance == claim.distance {
+                    claim.id = -2; // The "multiple equidistant" claim
+                } else if distance < claim.distance {
+                    claim.id = index as isize;
+                    claim.distance = distance;
+                }
+            }
+            if x == max_x || y == max_y || x == min_x || y == min_y {
+                infinite_area_ids.insert(claim.id);
+            }
+            if claim.id >= 0 {
+                *region_sizes.entry(claim.id as usize).or_insert(0) += 1;
+                map.insert(Position { x, y }, claim);
+            }
+        }
+    }
+
+    // let mut areas = HashMap::new();
+    // for claim in map
+    //     .values()
+    //     .filter(|claim| claim.id < 999 && !infinite_area_ids.contains(&claim.id))
+    // {
+    //     let area_entry = areas.entry(claim.id).or_insert(0usize);
+    //     *area_entry += 1;
+    // }
+    for x in &infinite_area_ids {
+        region_sizes.remove(&(*x as usize));
+    }
+    println!("{:?}", infinite_area_ids);
+    println!("{:?}", region_sizes);
+    println!("{:?}", region_sizes.iter().max_by_key(|&(_, v)| v).unwrap());
+}
+
+fn manhattan_distance(x1: isize, y1: isize, x2: isize, y2: isize) -> usize {
+    (x1 as isize - x2 as isize).abs() as usize + (y1 as isize - y2 as isize).abs() as usize
+}
+
+#[test]
+fn test_manhattan_distance() {
+    assert_eq!(manhattan_distance(0, 0, 1, 1), 2);
+    assert_eq!(manhattan_distance(0, 1, 1, 1), 1);
+    assert_eq!(manhattan_distance(1, 1, 1, 1), 0);
+    assert_eq!(manhattan_distance(1, 1, 3, 0), 3);
+}
