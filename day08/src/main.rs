2fcaa95c2bff16f53e03263ea71293690497e3d0
Orvar Segerström <orvarsegerstrom@gmail.com>
2018-12-08 07:12:57 +0100

    Day 8 part 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -7,16 +7,15 @@ fn main() {
         .map(|x| x.parse().unwrap())
         .collect();
 
-    let mut nodes: Vec<Node> = vec![];
-    let mut edges: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut nodes: HashMap<String, Node> = HashMap::new();
     {
         let mut stack: Vec<Frame> = vec![];
         stack.push(Frame {
-            id: 0,
+            id: "".to_string(),
             children_left: 1,
+            num_children: 1,
             num_metadata: 0,
         });
-        let mut id = 1usize;
         let mut index = 0;
         loop {
             let frame = stack.last_mut().expect("wat");
@@ -25,13 +24,14 @@ fn main() {
                 let num_metadata = input[index + 1];
                 index += 2;
 
-                let node_id = id;
-                id += 1;
+                let node_index = frame.num_children - frame.children_left;
+                let node_id = format!("{}->{}", frame.id, node_index);
 
                 frame.children_left -= 1;
                 stack.push(Frame {
                     id: node_id,
                     children_left: num_children,
+                    num_children,
                     num_metadata,
                 });
             } else {
@@ -40,10 +40,14 @@ fn main() {
                     metadata.push(input[index]);
                     index += 1;
                 }
-                nodes.push(Node {
-                    id: frame.id,
-                    metadata,
-                });
+                nodes.insert(
+                    frame.id.clone(),
+                    Node {
+                        id: frame.id.clone(),
+                        num_children: frame.num_children,
+                        metadata,
+                    },
+                );
                 stack.pop().unwrap();
                 if stack.len() < 1 {
                     break;
@@ -53,19 +57,56 @@ fn main() {
     }
     println!(
         "Part 1: {}",
-        nodes.iter().flat_map(|x| &x.metadata).sum::<usize>()
+        nodes.values().flat_map(|x| &x.metadata).sum::<usize>()
     );
-}
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-struct Frame {
-    id: usize,
-    children_left: usize,
-    num_metadata: usize,
+    nodes.get_mut("").unwrap().metadata = vec![1];
+
+    let mut values: HashMap<&str, usize> = HashMap::new();
+    loop {
+        if values.contains_key("") {
+            break;
+        }
+        'scan: for (id, node) in &nodes {
+            let id: &str = &*id;
+            if values.contains_key(id) {
+                continue;
+            }
+            if node.num_children > 0 {
+                let mut sum = 0;
+                for index in &node.metadata {
+                    if *index == 0 || *index > node.num_children {
+                        // skip
+                    } else {
+                        let sub_id: &str = &format!("{}->{}", id, index - 1);
+                        if let Some(value) = values.get(sub_id) {
+                            sum += value;
+                        } else {
+                            // node has no score yet
+                            continue 'scan;
+                        }
+                    }
+                }
+                values.insert(id, sum);
+            } else {
+                values.insert(id, node.metadata.iter().sum::<usize>());
+            }
+        }
+    }
+    println!("Part 2: {}", values.get("->0").unwrap());
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Node {
-    id: usize,
+    id: String,
+    num_children: usize,
     metadata: Vec<usize>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Frame {
+    id: String,
+    children_left: usize,
+    num_children: usize,
+    num_metadata: usize,
 }
