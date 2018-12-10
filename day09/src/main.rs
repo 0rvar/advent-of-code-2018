eaef14a34be97f5d038bf2a946ca120f8dd8ee39
Orvar Segerström <orvarsegerstrom@gmail.com>
2018-12-10 10:17:37 +0100

    Day 9: refactor to use double-ended queue

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,4 +1,4 @@
-use skiplist::SkipList;
+use std::collections::VecDeque;
 
 // input: 426 players; last marble is worth 72058 points
 const NUM_PLAYERS: usize = 426;
@@ -11,23 +11,26 @@ fn main() {
 
 fn max_score(max_marble: usize) -> usize {
     let mut scores = [0usize; NUM_PLAYERS];
-    let mut board: SkipList<usize> = SkipList::new();
+    let mut board: VecDeque<usize> = VecDeque::new();
     board.insert(0, 0);
 
     let mut current_player = 0;
-    let mut current_position = 0;
     for current_marble in 1..=max_marble {
         if current_marble % 23 == 0 {
-            let take_position = (current_position + board.len() - 7) % board.len();
-            let removed_marble = board.remove(take_position);
-
+            // Move 7 positions to the left
+            for _ in 0..7 {
+                let last = board.pop_back().unwrap();
+                board.push_front(last);
+            }
+            let removed_marble = board.pop_front().unwrap();
             scores[current_player] += current_marble;
             scores[current_player] += removed_marble;
-            current_position = take_position;
         } else {
-            let next_position = (current_position + 2) % board.len();
-            board.insert(current_marble, next_position);
-            current_position = next_position;
+            for _ in 0..2 {
+                let first = board.pop_front().unwrap();
+                board.push_back(first);
+            }
+            board.push_front(current_marble);
         }
         current_player = (current_player + 1) % NUM_PLAYERS;
     }
