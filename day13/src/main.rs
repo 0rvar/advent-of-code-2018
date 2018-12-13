b3e61a7d2851b26ed94baa86a41436c626e82eb8
Orvar Segerström <orvarsegerstrom@gmail.com>
2018-12-13 08:48:21 +0100

    Day 13 part 1

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -19,7 +19,7 @@ struct Cart {
 }
 
 fn main() {
-    let input: Vec<Vec<u8>> = include_str!("input2.txt")
+    let input: Vec<Vec<u8>> = include_str!("input.txt")
         .split("\n")
         .map(|x| x.as_bytes().iter().map(|x| *x).collect::<Vec<u8>>())
         .collect();
@@ -81,54 +81,96 @@ fn main() {
     }
 
     {
-        let mut carts = carts.clone();
+        let mut carts: Vec<Cart> = carts.clone();
         'it: for iteration in 1.. {
-            let next_carts = carts
-                .iter()
-                .map(|cart| {
-                    let next_position = move_in_direction(&cart.position, &cart.direction);
-                    let track = map.get(&next_position).unwrap();
-                    let mut next_turns = cart.turns;
-                    let next_direction = match track {
-                        Track::Horizontal | Track::Vertical => cart.direction.clone(),
-                        Track::Crossing => {
-                            next_turns += 1;
-                            choose_direction(&cart.direction, cart.turns)
-                        }
-                        Track::PositiveTurn => match cart.direction {
-                            // /
-                            Direction::North => Direction::East,
-                            Direction::East => Direction::North,
-                            Direction::South => Direction::West,
-                            Direction::West => Direction::South,
-                        },
-                        Track::NegativeTurn => match cart.direction {
-                            // \
-                            Direction::North => Direction::West,
-                            Direction::West => Direction::North,
-                            Direction::South => Direction::East,
-                            Direction::East => Direction::South,
-                        },
-                    };
-
-                    Cart {
-                        position: next_position,
-                        direction: next_direction,
-                        turns: next_turns,
+            carts.sort_by_key(|c| (c.position.x, c.position.y));
+            let mut next_carts: Vec<Cart> = vec![];
+            for (cart_index, cart) in carts.iter().enumerate() {
+                let next_position = move_in_direction(&cart.position, &cart.direction);
+                let track = map.get(&next_position).unwrap();
+                let mut next_turns = cart.turns;
+                let next_direction = match track {
+                    Track::Horizontal | Track::Vertical => cart.direction.clone(),
+                    Track::Crossing => {
+                        next_turns += 1;
+                        choose_direction(&cart.direction, cart.turns)
                     }
-                })
-                .collect::<Vec<Cart>>();
+                    Track::PositiveTurn => match cart.direction {
+                        // /
+                        Direction::North => Direction::East,
+                        Direction::East => Direction::North,
+                        Direction::South => Direction::West,
+                        Direction::West => Direction::South,
+                    },
+                    Track::NegativeTurn => match cart.direction {
+                        // \
+                        Direction::North => Direction::West,
+                        Direction::West => Direction::North,
+                        Direction::South => Direction::East,
+                        Direction::East => Direction::South,
+                    },
+                };
 
-            let mut cart_positions: HashSet<Position> = HashSet::new();
-            for cart in carts {
-                if cart_positions.contains(&cart.position) {
-                    println!("Part 1: {:?}", cart.position);
+                let collision_in_next = next_carts[0..cart_index]
+                    .iter()
+                    .find(|c| c.position == next_position)
+                    .is_some();
+                let collision_in_prev = carts[(cart_index + 1)..]
+                    .iter()
+                    .find(|c| c.position == next_position)
+                    .is_some();
+                if collision_in_next || collision_in_prev {
+                    println!("Part 1: {},{}", next_position.x, next_position.y);
                     break 'it;
                 }
-                cart_positions.insert(cart.position.clone());
+
+                next_carts.push(Cart {
+                    position: next_position,
+                    direction: next_direction,
+                    turns: next_turns,
+                });
             }
 
+            for y in 0..input.len() {
+                for x in 0..input[0].len() {
+                    let pos = Position {
+                        x: x as isize,
+                        y: y as isize,
+                    };
+                    if let Some(c) = next_carts.iter().find(|c| c.position == pos) {
+                        print!(
+                            "\x1b[1m\x1b[92m{}\x1b[0m",
+                            match c.direction {
+                                Direction::North => "^",
+                                Direction::South => "v",
+                                Direction::East => ">",
+                                Direction::West => "<",
+                            }
+                        );
+                    } else if let Some(t) = map.get(&pos) {
+                        print!(
+                            "{}",
+                            match t {
+                                Track::Horizontal => "-",
+                                Track::Vertical => "|",
+                                Track::Crossing => "+",
+                                Track::PositiveTurn => "/",
+                                Track::NegativeTurn => "\\",
+                            }
+                        );
+                    } else {
+                        print!(" ");
+                    }
+                }
+                println!();
+            }
+            println!("========================");
+            println!();
+
             carts = next_carts;
+
+            // use std::io;
+            // io::stdin().read_line(&mut String::new());
         }
     }
 }
@@ -139,7 +181,7 @@ fn choose_direction(dir: &Direction, counter: usize) -> Direction {
         Right,
         Forward,
     }
-    let turn = &[Turn::Left, Turn::Right, Turn::Forward][counter % 3];
+    let turn = &[Turn::Left, Turn::Forward, Turn::Right][counter % 3];
     match turn {
         Turn::Forward => dir.clone(),
         Turn::Left => match dir {
