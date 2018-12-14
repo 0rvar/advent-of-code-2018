cf849571ecb9a7611655c389682b75d1078eee09
Orvar Segerström <orvarsegerstrom@gmail.com>
2018-12-14 06:41:04 +0100

    Day 14 part 2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -2,6 +2,56 @@ fn main() {
     let input = 765071;
 
     println!("Part 1: {}", recipes_after(input));
+    println!("Part 2: {}", num_recipes_until(&input.to_string()));
+}
+
+fn num_recipes_until(recipe_match: &str) -> usize {
+    let mut a = 0;
+    let mut b = 1;
+    let mut recipes: Vec<usize> = vec![3, 7];
+    loop {
+        let current_a_recipe = recipes[a];
+        let current_b_recipe = recipes[b];
+        {
+            let mut new_recipes: Vec<usize> = (current_a_recipe + current_b_recipe)
+                .to_string()
+                .chars()
+                .map(|x| x.to_string().parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+
+            if recipes.len() > recipe_match.len() {
+                for i in 0..new_recipes.len() {
+                    let hypothetical_suffix = new_recipes
+                        .iter()
+                        .take(i)
+                        .map(|x| x.to_string())
+                        .collect::<String>();
+                    let before = recipes.len() - recipe_match.len() + i;
+                    let hypothetical_prefix = recipes
+                        .iter()
+                        .skip(before)
+                        .map(|x| x.to_string())
+                        .collect::<String>();
+                    let maybe_match = hypothetical_prefix + &hypothetical_suffix;
+                    if maybe_match == recipe_match {
+                        return before;
+                    }
+                }
+            }
+
+            recipes.append(&mut new_recipes);
+        }
+        a = (a + 1 + current_a_recipe) % recipes.len();
+        b = (b + 1 + current_b_recipe) % recipes.len();
+    }
+}
+
+#[test]
+fn test_num_recipes_until() {
+    assert_eq!(num_recipes_until("51589"), 9);
+    assert_eq!(num_recipes_until("01245"), 5);
+    assert_eq!(num_recipes_until("92510"), 18);
+    assert_eq!(num_recipes_until("59414"), 2018);
 }
 
 fn recipes_after(n: usize) -> String {
